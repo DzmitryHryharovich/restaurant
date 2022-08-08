fdf542ad8758f77b2209f0592dc30d39212e8a77
Дмитрий Dell <super.aezakmi93@yandex.ru>
2022-08-08 14:50:10 +0300

    08.08work

--- restaurant/Program.cs
+++ restaurant/Program.cs
@@ -17,7 +17,11 @@ namespace restaurant
             Console.WriteLine("****************************");
             Console.WriteLine("Открыть ресторан - O");
             Console.WriteLine("Рарегистрировать посетителя - N");
-            Console.WriteLine("Свободные места - F");
+            Console.WriteLine("Свободные места - PF");
+            Console.WriteLine("Свободные столики - TF");
+            Console.WriteLine("Добавить столики - A");
+            Console.WriteLine("Всего столиков в зале - T");
+            Console.WriteLine("Информация по все столикам - I");
             string choice = Console.ReadLine();
             if (choice == "O")
             {
@@ -33,9 +37,29 @@ namespace restaurant
                 if (isOpen == true) VisitorRegistration();
                 else Console.WriteLine(">>>Сначала нужно открыть ресторан!");
             }
-            else if (choice == "F")
+            else if (choice == "PF")
             {
-                if (isOpen == true) Console.WriteLine($"Свободных мест осталось: {FreePlacesCountAll()}");
+                if (isOpen == true) Console.WriteLine($"Свободных мест осталось: {FreePlacesCountAll(room)}");
+                else Console.WriteLine(">>>Сначала нужно открыть ресторан!");
+            }
+            else if (choice == "A")
+            {
+                if (isOpen == true) AddTables();
+                else Console.WriteLine(">>>Сначала нужно открыть ресторан!");
+            }
+            else if (choice == "T")
+            {
+                if (isOpen == true) Console.WriteLine("Всего столиков в зале: " + TablesCountAll(room));
+                else Console.WriteLine(">>>Сначала нужно открыть ресторан!");
+            }
+            else if (choice == "TF")
+            {
+                if (isOpen == true) TablesWithFreePlaces(room);
+                else Console.WriteLine(">>>Сначала нужно открыть ресторан!");
+            }
+            else if (choice == "I")
+            {
+                if (isOpen == true) AllTablesInfo(room);
                 else Console.WriteLine(">>>Сначала нужно открыть ресторан!");
             }
             else Console.WriteLine("!!!Неопознанный ввод");
@@ -49,69 +73,70 @@ namespace restaurant
             {
                 room.waiters.Add(new Waiter());
             }
-            Console.WriteLine("Сколько столиков в зале?");
-            int amountTables = Int32.Parse(Console.ReadLine());
-            for (int i = 0; i < amountTables; i++)
-            {
-                room.tables.Add(new Table(i + 1, 2));
-            }
+            AddTables();
             Console.WriteLine("Ресторан открыт! Добро пожаловать!");
             Console.WriteLine($"Свободных официантов - {room.waiters.Count}\nСвободных столиков - {room.tables.Count}");
         }
         public void VisitorRegistration()
         {
-            if (FreeTablesCountAll(room) < 1) Console.WriteLine("Нет свободных столиков!");
+            if (FreeTablesCount(room).Count < 1) Console.WriteLine("Нет свободных столиков!");
             else
             {
                 Console.WriteLine("Введите имя клиента");
                 visitors.Add(new Visitor(Console.ReadLine()));
-                Console.WriteLine($"Укажите номер столика за который посадить {visitors[visitors.Count-1].name}?");
-                Console.WriteLine($"Свободные столики:->" );
-                for (int i = 0; i < FreeTables(room).Count; i++)//??????????????????????????????
+                Console.WriteLine($"Укажите номер столика за который посадить {visitors[visitors.Count - 1].name}?");
+                TablesWithFreePlaces(room);
+                Console.WriteLine("Выберите столик:");
+            again: int choise = int.Parse(Console.ReadLine());
+                for (int i = 0; i < FreeTablesCount(room).Count; i++)
                 {
-                    Console.Write($"|{i+1} - {FreeTables(room)[i].id}| ");
+                    if (FreeTablesCount(room)[i].id == choise)
+                    { FreeTablesCount(room)[i].visitorsAtTable.Add(visitors[visitors.Count - 1]); break; }
+                    else if (i == FreeTablesCount(room).Count - 1) { Console.WriteLine("Столика с таким номером нет, либо за ним нет свободных мест! Повторите ввод:"); goto again; }
                 }
-                int choise = Int32.Parse(Console.ReadLine());
-                room.tables[choise-1].visitorsAtTable.Add(visitors[visitors.Count-1]);
-                Console.WriteLine($"Посититель {visitors[visitors.Count-1].name} - посажен за столик {FreeTables(room)[choise-1].id}!");
+                Console.WriteLine($"Посититель {visitors[visitors.Count - 1].name} - посажен за столик №{choise}!");
             }
         }
-        public int FreePlacesCountAll()
+        public void AddTables()
+        {
+            Console.WriteLine("Сколько столиков добавить?");
+            int amountTables = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько мест за каждым столиком?");
+            int amountPlaces = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= amountTables; i++)
+            {
+                if (room.tables.Count == 0) room.tables.Add(new Table(i, amountPlaces));
+                else room.tables.Add(new Table(room.tables[room.tables.Count - 1].id + 1, amountPlaces));
+            }
+        }
+        public int FreePlacesCountAll(Room room)
         {
             int amount = 0;
             for (int i = 0; i < room.tables.Count; i++)
             {
-                for (int j = 1; j < room.tables[i].places; j++)
-                {
-                    amount += room.tables[i].places - room.tables[i].visitorsAtTable.Count;
-                }
+                amount += room.tables[i].places - room.tables[i].visitorsAtTable.Count;
             }
             return amount;
         }
-        public int FreePlacesAtTable(Table table)
+        public int FreePlacesCount(Table table)
         {
-            int amount = 0;
-            for (int i = 0; i < table.places; i++)
-            {
-                if (table.visitorsAtTable.Equals(null)) amount++;
-            }
-            return amount;
+            return table.places - table.visitorsAtTable.Count;
         }
         public bool CheckTableIsBusy(Table table)
         {
-            if (FreePlacesAtTable(table) < table.places) return false;
+            if (FreePlacesCount(table) > 0) return false;
             else return true;
         }
-        public int FreeTablesCountAll(Room room)
+        public int TablesCountAll(Room room)
         {
             int amount = 0;
             for (int i = 0; i < room.tables.Count; i++)
             {
-                if (CheckTableIsBusy(room.tables[i]) == false) amount++;
+                amount++;
             }
             return amount;
         }
-        public List<Table> FreeTables(Room room)
+        public List<Table> FreeTablesCount(Room room)
         {
             List<Table> tables = new List<Table>();
             for (int i = 0; i < room.tables.Count; i++)
@@ -119,6 +144,29 @@ namespace restaurant
                 if (CheckTableIsBusy(room.tables[i]) == false) tables.Add(room.tables[i]);
             }
             return tables;
+        }
+        public void TablesWithFreePlaces(Room room)
+        {
+            Console.WriteLine($"Номера столиков с незанятыми местами:->");
+            for (int i = 0; i < FreeTablesCount(room).Count; i++)
+            {
+                Console.WriteLine($"|№{FreeTablesCount(room)[i].id}(свободных мест:{FreePlacesCount(FreeTablesCount(room)[i])}) | ");
+            }
+        }
+        public void AllTablesInfo(Room room)
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++");
+            for (int i = 0; i < room.tables.Count; i++)
+            {
+                int n = 1;
+                Console.WriteLine($"|Столик №{room.tables[i].id} | ");
+                foreach (var item in room.tables[i].visitorsAtTable)
+                {
+                    if (item == null) Console.WriteLine($"место №{n++} Пусто");
+                    else Console.WriteLine($"место №{i + 1} {item.name}");
+                }
+            }
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++");
         }
     }
 
@@ -136,11 +184,11 @@ namespace restaurant
     }
     public class Visitor : Human
     {
+        public Visitor() { }
         public Visitor(string name) { base.name = name; }
     }
     public class Table
     {
-        public Table() { }
         public Table(int id, int places)
         {
             this.id = id;
@@ -148,8 +196,8 @@ namespace restaurant
             visitorsAtTable = new List<Visitor>(places);
         }
         public List<Visitor> visitorsAtTable;
-        public int id { get; }
-        public int places { get;}
+        public int id { get; set; }
+        public int places { get; }
     }
     public class Room
     {
